da42e55483419f9a162abe1a49f23caca9f8f565
Ivan Tryhuba <74875731+ivantryhuba@users.noreply.github.com>
2021-05-14 21:44:36 +0300

    Переключение темы

--- src/theme.js
+++ src/theme.js
@@ -1,2 +1,34 @@
 const bodyEl = document.querySelector('body');
-const themeSwitch = document.querySelector('#theme-switch-toggle');
+const themeSwitcher = document.querySelector('#theme-switch-toggle');
+
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+
+themeSwitcher.addEventListener('change', themeSwitch);
+themeSwitcher.addEventListener('change', setLocalStorage);
+
+function themeSwitch() {
+  if (themeSwitcher.checked) {
+    bodyEl.classList.add(Theme.DARK);
+    bodyEl.classList.remove(Theme.LIGHT);
+  } else {
+    bodyEl.classList.add(Theme.LIGHT);
+    bodyEl.classList.remove(Theme.DARK);
+  }
+}
+
+function setLocalStorage() {
+  if (themeSwitcher.checked) {
+    localStorage.setItem('theme', Theme.DARK);
+  } else {
+    localStorage.removeItem('theme');
+    bodyEl.classList.setItem('theme', Theme.LIGHT);
+  }
+}
+
+if (localStorage.getItem('theme') === Theme.DARK) {
+  bodyEl.classList.add(Theme.DARK);
+  themeSwitcher.checked = true;
+}
